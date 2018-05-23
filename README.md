61fca888ac2bd61dc61ad1f90375ec51537f903d
Jérémie Girault <jeremie.girault@gmail.com>
2018-05-23 15:56:56 +0200

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# ESLint config for Hubvisor projects
+
+## Installation
+
+`yarn install @hubvisor/estlint-config-hubvisor -D`
+
+## Usage
+
+In your .estintrc.js :
+
+```
+module.exports = {
+  extends: [ "@hubvisor/hubvisor" ]
+}
+```
---
+++ index.js
@@ -0,0 +1,26 @@
+module.exports = {
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module'
+  },
+  extends: [
+    'plugin:vue/strongly-recommended',
+    'standard'
+  ],
+  plugins: [ 'vue' ],
+  rules: {
+    'multiline-ternary': [ 'error', 'never' ],
+    'object-curly-spacing': [ 'error', 'always' ],
+    'array-bracket-spacing': [ 'error', 'always', {
+      'singleValue': true,
+      'objectsInArrays': true,
+      'arraysInArrays': true
+    } ],
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+  }
+}
