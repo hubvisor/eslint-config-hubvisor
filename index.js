470e9dd8f805920ee6ad3770f1a0a8c2a7472f0c
Jérémie Girault <jeremie.girault@gmail.com>
2019-05-02 18:16:22 +0200

    enh: add typescript support

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 module.exports = {
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: '@typescript-eslint/parser',
     sourceType: 'module'
   },
   extends: [
+    // 'plugin:@typescript-eslint/recommended',
     'plugin:vue/strongly-recommended',
     'standard'
   ],
-  plugins: [ 'vue' ],
+  plugins: [
+    '@typescript-eslint',
+    'vue'
+  ],
   rules: {
     'multiline-ternary': [ 'error', 'never' ],
     'object-curly-spacing': [ 'error', 'always' ],
@@ -21,13 +25,41 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+
+    // typescript rules
+    '@typescript-eslint/indent': [ 'error', 2 ],
+    '@typescript-eslint/member-delimiter-style': [ 'error', {
+      multiline: {
+          delimiter: 'none',
+          requireLast: false
+      },
+      singleline: {
+          delimiter: 'comma',
+          requireLast: false
+      }
+    } ],
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/array-type': [ 'error', 'array-simple' ],
+    '@typescript-eslint/explicit-member-accessibility': [ 'error', {
+      overrides: {
+        constructors: 'no-public'
+      }
+    } ],
+    '@typescript-eslint/no-unused-vars': [ 'error', {
+      args: 'none'
+    } ],
+    '@typescript-eslint/no-non-null-assertion': 'off', // this syntax is already a warning !
+    '@typescript-eslint/camelcase': [ 'error', {
+      properties: 'never' // we need to enforce it
+    } ]
   },
   overrides: [ {
     files: [
-      "**/__tests__/**/*.js",
-      "**/*.test.js",
-      "**/*.spec.js"
+      '**/__tests__/**/*.{js,ts}',
+      '**/*.test.{js,ts}',
+      '**/*.spec.{js,ts}'
     ],
     env: {
       jest: true
