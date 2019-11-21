52a1289d3c69b3b93419653ea3820d98d6ed2c29
Jérémie Girault <jeremie.girault@gmail.com>
2019-11-21 15:03:04 +0100

    fix: add an exception for js lint in ts triggering a false negative when using null-coalescing operators

--- index.js
+++ index.js
@@ -68,6 +68,7 @@ module.exports = {
   }, {
     files: [ '*.ts', '*.tsx' ],
     rules: {
+      'no-undef': 'off', // check https://github.com/typescript-eslint/typescript-eslint/issues/1116 and remove on upgrade if needed
       '@typescript-eslint/explicit-member-accessibility': [ 'error', { overrides: { constructors: 'no-public' } } ]
     }
   } ]
